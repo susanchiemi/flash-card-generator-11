647b4ba7f3fc25ee558ad71931bbbf69f96f3954
susanchiemi <Wormley5@aol.com>
2017-07-25 14:54:30 -0400

    commit

--- basicCard.js
+++ basicCard.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Require the inquirer package
 var inquirer = require('inquirer');
 var questions = require('./questions.js').questions;
@@ -9,14 +8,3 @@ exports.basicCard = function(full, cloze) {
 }
 
 
-=======
-//Require the inquirer package
-var inquirer = require('inquirer');
-var questions = require('./questions.js').questions;
-
-basicCard = function(front, back) {
-    this.front = front;
-    this.back = back;
-}
-
->>>>>>> refs/remotes/origin/master
