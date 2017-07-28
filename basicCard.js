ecfa2371923de1fa4367649629c54ff9442cc760
susanchiemi <Wormley5@aol.com>
2017-07-28 11:21:09 -0400

    still trying

--- basicCard.js
+++ basicCard.js
@@ -1,30 +1,56 @@
+// I was trying to make a flashcard app that shows one question at a time with a prompt for the answer.  
+
+
+
+
 //Require the inquirer package
 var inquirer = require('inquirer');
 //Import full list of questions
 var questions = require('./questions.js').questions;
 
-    exports.basicCard = function(front, back) {
+function BasicCard(front, back) {
     this.front = front;
     this.back = back;
+} 
 
+
+//Variable that holds the question
+ var fullQuestion = [];
+
+
+//Show first question (front) -- not working
+for (var i = 0; i < questions.length; i++) {
+    var Q = new BasicCard(questions[i].front);
+    fullQuestion.push(Q); 
+   // console.log(questions);
 }
 
+var currentQuestion = 0;
+var answerRight = 0;
+var answerWrong = 0;
 
-//Variable that holds the cloze question
-var clozeQuestions = [];
+function askQuestion () {
+    inquirer.prompt ([
+        {
+            type: "input",
+            message: fullQuestion[currentQuestion].full,
+            name: "userGuess"
+        }
+    ])
+    
+}
 
-/*  for (var i = 0; i < questions.length; i++) {
-    var ques = new basicCard.clozeCard(questions[i].full, questions[i].cloze);
-    clozeQuestions.push(ques); 
+//Prompt for answer 
 
-    this.full = text;
-    this.cloze = cloze;
-    this.partial = text.replace(cloze, ". . .");
 
-} */
 
-//console.log(clozeQuestions);
-console.log(questions);
+//Show full answer (back)
+
+//Next Question
+//Repeat
+
+
+//console.log(questions);
 
 
 
